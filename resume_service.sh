d81c19ec22b7c4335f5393527a6daf00d2d15d27
ejiang-airta <service@airta.co>
2025-05-13 23:19:00 -0700

    Update the resume servie script

--- resume_service.sh
+++ resume_service.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # resume_service.sh
 
-# Required ENV variables
-API_KEY="${RENDER_API_KEY}"
-SERVICE_ID="${FRONTEND_SERVICE_ID}"
-
-# API endpoint
-curl -X POST \
-  -H "Authorization: Bearer $API_KEY" \
-  https://api.render.com/v1/services/$SERVICE_ID/resume
+curl -X POST "https://api.render.com/v1/services/${FRONTEND_SERVICE_ID}/resume" \
+     -H "Authorization: Bearer ${RENDER_API_KEY}"
