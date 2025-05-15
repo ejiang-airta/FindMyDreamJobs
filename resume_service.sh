5061d89a4840a3f0287ffcb00359f0aef4c93d7e
ejiang-airta <service@airta.co>
2025-05-14 19:25:43 -0700

    Add wake backend_service

--- resume_service.sh
+++ resume_service.sh
@@ -3,3 +3,6 @@
 
 curl -X POST "https://api.render.com/v1/services/${FRONTEND_SERVICE_ID}/resume" \
      -H "Authorization: Bearer ${RENDER_API_KEY}"
+
+curl -X POST "https://api.render.com/v1/services/${BACKEND_SERVICE_ID}/resume" \
+     -H "Authorization: Bearer ${RENDER_API_KEY}"
