1a28d2031e6e719877bb7b3b95bef83d8c7a9530
ejiang-airta <service@airta.co>
2025-05-20 12:13:34 -0700

    Added integration of Google Tag Manager with Next.js/App Router

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,20 +24,29 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* ✅ Google Analytics Tag */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-152EW46P4X"></script>
+        {/* Google Tag Manager head script*/}
         <script
           dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K7DB35BN');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className={inter.className}>
+        {/* Google Tag Manager (noscript) for body */}
+        <noscript
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-152EW46P4X');
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7DB35BN"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
         />
-      </head>
-      <body className={inter.className}>
         <SessionWrapper>
           <NavBar />
           {children}
